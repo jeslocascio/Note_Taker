52d7503d4a07300f5011a08e8906b5c51a607839
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2023-09-11 16:04:43 -0400

    Added router for notes, as well as added the get/post request for the notes to appear on the screen

---
+++ routes/notes.js
@@ -0,0 +1,31 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+notes.get("/", (req, res) => {
+    readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+  });
+
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+
+        const reponse = {
+            status: 'success',
+            body: newNote,
+        }
+
+        res.json(response);
+    } else {
+        res.error('Error added new Note');
+    }
+})
+module.exports = notes;
